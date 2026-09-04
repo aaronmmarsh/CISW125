8584dac0e01459fd9b7e706ca6c0c24d684b0a56
aaronmmarsh <aaronmelmarsh@gmail.com>
2026-09-03 19:43:53 -0600

    Both Projects for Project0 assignment using variables.

---
+++ Week2/Project0_1.py
@@ -0,0 +1,6 @@
+# Aaron Marsh
+
+morning_act=input("so whatcha doing this morning?:")
+print(f"ohh so you're gonna be {morning_act} this morning, sounds like fun!")
+morning_act=input("so just to make sure you're gonna be doing what?:") 
+print(f"ok ok I get it now you're gonna be {morning_act} today, sorry for the confusion!")
